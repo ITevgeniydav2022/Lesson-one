e2b887996a2dfdcafb031ad7d3ab268098d7b83e
evgeniydavydov <defox2022@gmail.com>
2025-07-07 22:41:04 +0300

    Completed the homework for the lesson alone

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Hallo Женя!");
+        System.out.print(1);
+        System.out.println(3);
+        System.out.println(5);
+        System.out.print(4);
+
     }
 }
